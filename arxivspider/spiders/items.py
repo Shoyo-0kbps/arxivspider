f4a226c0597335adf861d4920787f49d8b941080
Shoyo-0kbps <mizzaelsantos1@gmail.com>
2021-12-27 20:50:28 -0300

    get all caterories and links from arXiv.org

---
+++ arxivspider/spiders/items.py
@@ -0,0 +1,14 @@
+import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import MapCompose, TakeFirst
+from w3lib.html import remove_tags
+
+
+class ArxivscraperItem(scrapy.Item):
+    
+    category_name = scrapy.Field(input_processor = MapCompose(remove_tags),
+                                 output_processor = TakeFirst())
+    latest_name = scrapy.Field(input_processor = MapCompose(remove_tags))
+    latest_link = scrapy.Field()
+    subcategory_name = scrapy.Field(input_processor = MapCompose(remove_tags))
+    subcategory_link = scrapy.Field()
---
+++ arxivspider/spiders/parsers/arxiv_base.py
@@ -0,0 +1,24 @@
+import scrapy
+from scrapy.loader import ItemLoader
+from arxivspider.spiders.items import ArxivscraperItem
+
+class ArxivSpider(scrapy.Spider):
+    name = 'arxiv_base'
+    
+    start_urls = ['https://arxiv.org/']
+
+    def parse(self, response):
+       
+        for topics in response.xpath(
+                    '//div[contains(@id,"content")]//ul[preceding-sibling::h2]//li')[:-5]:
+               
+                l = ItemLoader(item=ArxivscraperItem(), selector=topics)
+                latest_loader = l.nested_xpath('.//a[position() >= 2 and (position() <= 3)]')
+                
+                l.add_xpath('category_name', './/a')
+                latest_loader.add_xpath('latest_name','.')
+                latest_loader.add_xpath('latest_link', './@href')
+                l.add_xpath('subcategory_name', './/a[preceding-sibling::br and not(text()="detailed description")]')
+                l.add_xpath('subcategory_link', './/a[preceding-sibling::br]/@href[not(contains(.,"help"))]')
+                
+                yield l.load_item()
